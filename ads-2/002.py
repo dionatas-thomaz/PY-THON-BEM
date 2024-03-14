259116419da2f92fba59e334d447f25502ddb6b4
dionatas-thomaz <dionatassanto0@gmail.com>
2024-03-14 12:21:45 -0300

    curso py

---
+++ ads-2/002.py
@@ -0,0 +1,2 @@
+nome=input("digite o seu nome:")
+print("e um prazer te conhecer,",nome)
---
+++ ads-2/if-else.py
@@ -0,0 +1,6 @@
+print("o heroi que buscamos e o supermen, responda apenas s/n")
+reposta1=input("esse personagem tem capa?")
+if reposta1 == "n" :
+    print("entao nao e a mulher maravilha")
+else:
+    reposta2=input("esse personagem tem um S no peito ?")
